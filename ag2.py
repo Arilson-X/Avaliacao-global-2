5d7b6249cdb6aa2cdf0acfb14726a4979d308cf4
Arilson-X <arilsonouzacosta0@gmail.com>
2022-06-18 14:16:58 -0300

    Implementado metodo de conversao dos dados

--- ag2.py
+++ ag2.py
@@ -1,1 +1,14 @@
-import  pandas as pd
+import pandas as pd
+from sklearn.linear_model import Perceptron
+
+
+def tratadados(dfname):
+    df_dados = pd.read_csv(dfname, sep=',')
+    df_dados = df_dados.replace('o', -1)
+    df_dados = df_dados.replace('b', 0)
+    df_dados = df_dados.replace('x', +1)
+    df_dados = df_dados.replace('negativo', -1)
+    df_dados = df_dados.replace('positivo', +1)
+
+    df_dados.to_csv("dados_tratados.csv")
+
