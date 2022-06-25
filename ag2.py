c3cc4fe518e7260d1d913c8d15c99c3462d7ff73
Arilson-X <arilsonouzacosta0@gmail.com>
2022-06-25 13:42:10 -0300

    atualizado o projeto com metricas as metricas de avaliação

--- ag2.py
+++ ag2.py
@@ -1,10 +1,30 @@
 import pandas as pd
-import numpy as np
-import matplotlib
 
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+
+
+def verificaresultado(resultado):
+    if resultado == 1:
+        print('X ganhou')
+    else:
+        print('X perdeu')
+
+
+def mostravelha(entrada):
+    velha = []
+    for j in range(0, 9):
+        if entrada[j] == 1:
+            velha.append('X')
+        elif entrada[j] == 0:
+            velha.append(' ')
+        elif entrada[j] == -1:
+            velha.append('O')
+    print(f'|{velha[0]}|{velha[1]}|{velha[2]}| \n'
+          f'|{velha[3]}|{velha[4]}|{velha[5]}| \n'
+          f'|{velha[6]}|{velha[7]}|{velha[8]}|')
 
 
 def tratadados(dfname):
@@ -14,6 +34,7 @@ def tratadados(dfname):
     df_dados = df_dados.replace('x', +1)
     df_dados = df_dados.replace('negativo', -1)
     df_dados = df_dados.replace('positivo', +1)
+    df_dados = df_dados.sort_index(ascending=False)
 
     df_dados.to_csv("dados_tratados.csv", index=False)
     print(df_dados)
@@ -21,17 +42,33 @@ def tratadados(dfname):
 
 tratadados('dados.csv')
 df = pd.read_csv("dados_tratados.csv")
-X = np.array(df.iloc[:, :9])
-print(X.shape)
-Y = np.array(df.iloc[:, 9].values)
-print(Y.shape)
+X = df.iloc[:, :9]
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.8, random_state=2)
-classif = Perceptron(penalty='elasticnet').fit(x_train, y_train)
+print(X)
+Y = df.iloc[:, 9]
+print(type(Y))
+print(Y)
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.8)
+classif = Perceptron(tol=1e-3, random_state=0).fit(x_train, y_train)
+
 score = classif.score(x_test, y_test)
-resultado = classif.predict([[1, -1, 1, 1, -1, -1, -1, 1, 1]])
-print(score)
-print(resultado)
+print(f'Score: {score}')
+y_score = classif.predict(x_test)
+accuracy = accuracy_score(y_test, y_score, normalize=False)
+print(f'Accuracy: {accuracy} de {len(y_test)}')
+recall_score = recall_score(y_test, y_score)
+print(f'Recall Score: {recall_score}')
+
+entrada = []
+for i in range(0, 9):
+    entrada.append(int(input(f"Entre com o valor para a {i+1}ª posição: \n")))
+
+mostravelha(entrada)
+
+resultado = classif.predict([entrada])
+
+print(verificaresultado(resultado[0]))
 
 
 
